3b8bad5b4fa9183f29367e7f39b2e8881846a3ae
inhere <in.798@qq.com>
2019-02-26 23:05:03 +0800

    update some info. add some bootstrap file for tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,6 @@
 <?php
-
-require dirname(__DIR__) . '/vendor/autoload.php';
+// Composer autoload
+$autoloadFile = dirname(__DIR__) . '/vendor/autoload.php';
+if (file_exists($autoloadFile)) {
+    require_once $autoloadFile;
+}
